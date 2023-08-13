6b297c9ee1aa7d0f1a29442587bd5424915edb26
Durgesh1438 <durgesh.kovvuri1438@gmail.com>
2023-08-13 11:22:09 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eb84558d-9bc2-42ea-92d1-e8e2e17097d0';
+export const MODAL_CLOSE_SEC = 2.5;
